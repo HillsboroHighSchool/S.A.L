938188c4d37fc51e718117f7d02df61443a16dfc
HillsboroHighSchool <102313784+HillsboroHighSchool@users.noreply.github.com>
2022-03-27 22:31:01 -0500

    Update Test.js

--- src/Test.js
+++ src/Test.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch')
 const util = require('util')
 
-fetch('https://maps.googleapis.com/maps/api/directions/json?origin=4710+Dakota+ave&destination=3812+Hillsboro+pike&key=AIzaSyCbwuhNvOJQrYWnLRF6WjzJeOcnhYYfpZA')
+fetch('nope')
     .then(res => res.json()) // the .json() method parses the JSON response into a JS object literal
     .then(data => console.log(util.inspect(data.routes.inspect, true, 10)));
