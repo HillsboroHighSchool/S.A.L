682442bd8106b26404be5765f2775ec7e2e33bbc
HillsboroHighSchool <102313784+HillsboroHighSchool@users.noreply.github.com>
2022-03-29 20:05:51 -0500

    Fixed

--- src/Directions.js
+++ src/Directions.js
@@ -1,9 +1,17 @@
-const request = require('request')
-const dotenv = require('dotenv').config()
-const url = process.env.MAPS_API
+var axios = require('axios');
 
-request(url, (error, response, body) => {
-    const data = JSON.parse(body)
-    const Output = JSON.parse((JSON.stringify(data.routes)))
-    console.log(Output)
-})
+var config = {
+    method: 'get',
+    url: `https://maps.googleapis.com/maps/api/distancematrix/json?origins=4710+dakota+ave&destinations=${process.argv[2]}&units=imperial&key=AIzaSyCbwuhNvOJQrYWnLRF6WjzJeOcnhYYfpZA`,
+    headers: {}
+};
+
+axios(config)
+    .then(function(response) {
+        var a = (JSON.stringify(response.data.rows));
+        const Directions = a.slice(79, 94);
+        console.log(Directions)
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
